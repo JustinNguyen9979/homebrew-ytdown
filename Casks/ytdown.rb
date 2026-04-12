572b059d78e4d2534c2aa3aa7f29b9f89172186b
GitHub Action <JustinNguyen9979@github.com>
2026-04-12 18:04:54 +0700

    Update ytdown to 2026.04.12

--- Casks/ytdown.rb
+++ Casks/ytdown.rb
@@ -1,6 +1,6 @@
 cask "ytdown" do
   version "2026.04.12"
-  sha256 "6ac48d2bb9371e2c95f9450f7034e3d4978ba6cae7ef5ce4c611ef1813eb65d4"
+  sha256 "e20182c8aaa4ae2788c6189c5c805ed57822c110b1c055b4f12829cf5e7225ed"
 
   url "https://github.com/JustinNguyen9979/YTDown/releases/download/#{version}/YTDown-#{version}.dmg"
 
